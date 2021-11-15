00aaf000f2c2295618d1c2fc9bf9622a95110b7f
wetss <83474873+wetss@users.noreply.github.com>
2021-11-15 10:46:29 -0500

    Create admin.php

---
+++ admin.php
@@ -0,0 +1,16 @@
+
+<?php 
+
+ ?>
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<meta charset="utf-8">
+ 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+ 	<title>Panel Administrativo</title>
+ 	<link rel="stylesheet" href="">
+ </head>
+ <body>
+ 	<h2>Bienvenido administrador 😄😄</h2>
+ </body>
+ </html>
